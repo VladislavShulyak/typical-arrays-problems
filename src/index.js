a01182cf7fa0d3f65bccbb50ae44a97d7466b15e
Vladislav Shulyak <vladislav.dll@gmail.com>
2020-02-19 21:51:56 +0300

    feat: get a solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+exports.min = function min(array) {
+    if (!array) {
+        return 0
+    } else if (array.length === 0) return array;
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min)
+            min = array[i];
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array) {
+        return 0
+    } else if (array.length === 0) return array;
 
-exports.max = function max (array) {
-  return 0;
-}
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max)
+            max = array[i];
+    }
+    return max;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array) {
+        return 0
+    } else if (array.length === 0) return array;
+
+    let avg = 0;
+    for (let i = 0; i < array.length; i++) {
+        avg += array[i];
+
+    }
+
+    return (avg / array.length);
+};
